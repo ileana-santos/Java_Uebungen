b43541b71a561b7905c1986389e17364f91319eb
ileana-santos <isanvera@posteo.net>
2023-04-27 10:12:17 +0200

    Create columns and rows
    Code for creating colums and rows using multiples For Loops Statement

---
+++ Ablaufkontrolle/MulFor01.java
@@ -0,0 +1,14 @@
+public class MulFor01 {
+    public static void main(String[] args) {
+
+        //Multiple For Loops
+        //Columns and Rows
+        for (int i = 0; i < 5; i++) {
+
+            for( int j = 0; j <  4; j++) {
+                System.out.print("i= " + i+ " j= " + j + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
