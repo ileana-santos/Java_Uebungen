3e08ea70c7a3e51a106c733fb92794057368e1c5
ileana-santos <isanvera@posteo.net>
2023-08-14 15:36:01 +0200

    Reviewing the concept Inheritance

--- IndeJAVALearning/src/Inheritance/Dog.java
+++ IndeJAVALearning/src/Inheritance/Dog.java
@@ -1,7 +1,5 @@
 package Inheritance;
 
-import Inheritance.Animal;
-
 public class Dog extends Animal {
     private String earShape;
     private String tailShape;
@@ -28,12 +26,40 @@ public class Dog extends Animal {
     }
 
     public void makeNoise(){
-
+        if (type == "Wolf") {
+            System.out.println("Ow Wooooo!");
+        }
+        bark();
+        System.out.println();
     }
 
     @Override
     public void move(String speed) {
         super.move(speed);
-        System.out.println("Dogs walk, run and wag their tail");
+  //      System.out.println("Dogs walk, run and wag their tail");
+        if (speed == "slow"){
+            walk();
+            wagTail();
+        } else {
+            run();
+            bark();
+        }
+        System.out.println();
+    }
+
+    private void  bark(){
+        System.out.println("Woof! ");
+    }
+
+    private void  run(){
+        System.out.println("Dog Running ");
+    }
+
+    private void  walk(){
+        System.out.println("Dog Walking ");
+    }
+
+    private void  wagTail(){
+        System.out.println("Tail Waging");
     }
 }
